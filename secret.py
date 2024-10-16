c57be6f25bdf9a3c95c4aa54e2df4c2339bea224
mattAsant223 <149116196+mattAsant223@users.noreply.github.com>
2024-10-16 15:42:07 -0500

    Update secret.py

--- secret.py
+++ secret.py
@@ -1,8 +1,8 @@
 # For the YouTube Data API
-PLAYLIST_ID = "PLxyh2cq3b7mU21dTOOvMVcdse4Il8rC_c"
+PLAYLIST_ID = "#############################"
 
 # For the Spotify Web API
 CLIENT_ID = "e3f90b9d1a6d4ddfb99600fd92b13296"
 CLIENT_SECRET = "#############################"
-USER_ID = "xzbj1ospyogi64xn08a8j8mhk"
+USER_ID = "#############################"
 REDIRECT_URI = "https://www.google.com/"
